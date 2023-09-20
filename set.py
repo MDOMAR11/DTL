8415d102a1ad08afb166f500aaf70d0a31d1e810
MDOMAR11 <143377166+MDOMAR11@users.noreply.github.com>
2023-09-20 04:29:40 -0700

    Update set.py

--- set.py
+++ set.py
@@ -1,1 +1,33 @@
-# update set itemsthisset = {"apple", "banana", "cherry"}uniq = {"leoanl", "messi","Ronaldo "}thisset.update(uniq)print(thisset)# sete items is not duplicate value allowfirst= {1,2,3,4,5,6,7,8}second = {4,5,6,7,8}both =first|secondprint(both)# just common value asigncommon = first & secondprint(common)# print key methodB = {    "django":12,    "project":8,    "students":20}value =B.keys()print(value)
+# update set items
+thisset = {"apple", "banana", "cherry"}
+uniq = {"leoanl", "messi","Ronaldo "}
+thisset.update(uniq)
+print(thisset)
+
+# sete items is not duplicate value allow
+first= {1,2,3,4,5,6,7,8}
+second = {4,5,6,7,8}
+both =first|second
+print(both)
+
+# just common value asign
+common = first & second
+print(common)
+
+# print key method
+B = {
+    "django":12,
+    "project":8,
+    "students":20
+}
+value =B.keys()
+print(value)
+
+# even ,odd and zero check
+num = float(input("Enter your number:"))
+if (num==0):
+    print(f"this is a zero{num}")
+elif (num%2==0):
+    print("this is a even number")
+else:
+    print("this is a odd number ")
